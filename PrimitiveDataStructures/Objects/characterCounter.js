04dd8fda66a539a9e45703e1844bcc87faa5d2a5
Martin Sung <martinsung00@gmail.com>
2020-03-16 21:18:38 -0700

    Add character counter to objects.

---
+++ PrimitiveDataStructures/Objects/characterCounter.js
@@ -0,0 +1,23 @@
+/*
+Create a function to count all characters within a string.
+*/
+
+function countAllCharacters(string) {
+// Create an object to store key (representing the character) and value (representing the count).
+    let frequencies = {};
+    for (i = 0; i < string.length; i++) {
+// The current letter variable will represent the letter which we are at in the interation.
+      let currentLetter = string[i]
+// If the character is missing in the object frequencies, we give it a value of 1 (first time we've seen it)
+      if (frequencies[currentLetter] === undefined) {
+        frequencies[currentLetter] = 1;
+      } else {
+// Or else we let our counter equal the value (current number of times we have seen it) and add 1 to the counter.
+        let count = frequencies[currentLetter];
+        frequencies[currentLetter] = count + 1;
+      }
+    }
+    return frequencies;
+  }
+  
+  console.log(countAllCharacters('banana'));
